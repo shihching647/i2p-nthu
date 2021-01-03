823958aaf5ee54931b15be23033e244be029aff7
scliou <scliou@fontrip.com>
2021-01-03 18:26:16 +0800

    Initial Commit

---
+++ i2p-nthu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  i2p-nthu
+//
+//  Created by 劉仕卿 on 2021/1/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
