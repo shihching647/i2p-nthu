c64f563dc12623cdc972a65ef14cd30eac01bf2a
shihching-home <shihching647@gmail.com>
2021-01-24 20:47:11 +0800

    problem 12129

---
+++ Part1_contest1/12129 - Do you know de way.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int l, r;
+
+    scanf("%d%d", &l, &r);
+    if(l > r) {
+        int temp = l;
+        l = r;
+        r = temp;
+    }
+    if(2 * l <= r)
+        printf("%d %d\n", l ,2 * l);
+}
