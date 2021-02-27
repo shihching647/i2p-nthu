c0417acf7867ec3622fa76bc38d9ce5fa212cb3e
scliou <ariel647@hotmail.com>
2021-02-27 17:28:05 +0800

    [Week6] Pick jewels

--- Part1/Week6/car_pick_jewels.c
+++ Part1/Week6/car_pick_jewels.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAP_SIZE 12
 #define CAR_SIZE 3
-#define ONLINE_JUDGE
+//#define ONLINE_JUDGE
 
 int map[MAP_SIZE][MAP_SIZE];
 void map_reset(void);
@@ -17,12 +17,14 @@ void jewels_read(void);
 void jewels_put_on_map(void);
 
 int car[CAR_SIZE][CAR_SIZE] = {{'O', 'O', '@'}, {'O', 'O', 'O'}, {'O', 'O', '@'}};
-int car_row = 3, car_col = 4;
+int car_row = 3, car_col = 4; //初始位置
+int speed = 1;
 int car_direction; //0:右, 1:下, 2:左, 3:上
 int car_earnings;
 void car_rotate90(void);
 void car_put_on_map(void);
 void car_move(void);
+void upgrade(void);
 
 int main(void)
 {
@@ -40,12 +42,10 @@ int main(void)
     while ((ch=getchar()) != EOF) {
         if (ch == '\n') continue; //避免讀到'\n'多印一次map
         if (ch=='R') {
-
             car_rotate90();
         }
 
         if (ch=='F') {
-
             car_move();
         }
 
@@ -57,9 +57,9 @@ int main(void)
 
         car_put_on_map();
 
-//#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
         map_show();
-//#endif
+#endif
     }
 
 
@@ -107,7 +107,7 @@ void jewels_read(void)
     scanf("%d", &n);
     for (i=0; i<n; i++) {
         scanf("%d%d", &row, &col);
-        jewels[row][col] = 1;
+        jewels[row][col]++;
     }
 }
 
@@ -140,9 +140,10 @@ void jewels_put_on_map()
 
         for (j=0; j<MAP_SIZE; j++) {
 
-            if (jewels[i][j])
-
+            if (jewels[i][j] == 1)
                 map[i][j] = '$';
+            else if (jewels[i][j] > 1)
+                map[i][j] = 'S';
         }
     }
 }
@@ -233,7 +234,7 @@ void car_rotate90(void)
 }
 
 
-int isLegalMove(int row, int col) {
+int is_legal_move(int row, int col) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (blocks[row + i][col + j] || map[row + i][col + j] == 'H' )
@@ -241,6 +242,38 @@ int isLegalMove(int row, int col) {
         }
     }
     return 1;
+}
+
+void reset_jewels(int row, int col) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (jewels[row + i][col + j] < 0) {
+                jewels[row + i][col + j] *= -1; //把寶物的值改回正的
+            }
+        }
+    }
+}
+
+void pick_jewels(int row, int col) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (jewels[row + i][col + j] > 0) {
+                jewels[row + i][col + j]--;
+                jewels[row + i][col + j] *= -1; //超過一個寶物時, 改成負值
+                car_earnings++;
+            }
+        }
+    }
+}
+
+void upgrade() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if(car[i][j] == '@') car[i][j] = 'X';
+            if(car[i][j] == 'O') car[i][j] = '*';
+        }
+    }
+    speed = 2;
 }
 
 /*
@@ -253,13 +286,22 @@ void car_move(void)
     // 分別對上 0:右, 1:下, 2:左, 3:上, 取代if else 的寫法
     int row_dict[] = {0, 1, 0, -1};
     int col_dict[] = {1, 0, -1, 0};
-    car_row_try = car_row_try + row_dict[car_direction];
-    car_col_try = car_col_try + col_dict[car_direction];
-    if (isLegalMove(car_row_try, car_col_try)) {
+    int row_delta = row_dict[car_direction] * speed;
+    int col_delta = col_dict[car_direction] * speed;
+    car_row_try = car_row_try + row_delta;
+    car_col_try = car_col_try + col_delta;
+    if (is_legal_move(car_row_try, car_col_try)) {
         car_row = car_row_try;
         car_col = car_col_try;
+        //當車子撿寶物上時, 把有超過一個寶物的位置的值改成負值, 當車子離開寶物時,再reset_jewels(把負值改成正值)
+        reset_jewels(car_row - row_delta, car_col - col_delta);
+        pick_jewels(car_row, car_col);
+        //upgrade
+        if (car_earnings >= 1 && speed == 1) {
+            car_earnings -= 1;
+            upgrade();
+        }
     }
-    
 }
 
 
